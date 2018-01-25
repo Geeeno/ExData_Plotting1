aa8e0bbbd34f4068a3a0fc83d5bf1a028dddac44
Geeeno <a.pelle@live.it>
2018-01-25 16:56:03 +0100

    Course Poject 1 25/01/2018 - modified .R files

--- plot1.R
+++ plot1.R
@@ -1,13 +1,33 @@
 plot.1 <- function(file.name = "household_power_consumption.txt"){
+    
+    # Check if needed libraries are loaded
+    
     if ("dplyr" %in% rownames(installed.packages()) == FALSE){
         install.packages("dplyr")
     }
     library(dplyr)
+    
+    # Check if there are correct file
+    
+    file.name <- "./household_power_consumption.txt"
+    if(!file.exists(file.name)){
+        if(file.exists("./exdata.zip")){
+            unzip("./exdata.zip")
+        }else{
+            stop("Missed files")
+        }
+    }
+    
+    # Load data
+    
     electric.data <- read.table(file.name, header = TRUE, sep = ";", 
                                 na.strings = "?", stringsAsFactors = FALSE)
     electric.data$Date <- as.Date(electric.data$Date, "%d/%m/%Y")
     electric.data <- filter(electric.data, Date >= "2007-02-01" & 
                                 Date <= "2007-02-02")
+    
+    # Create Plot
+    
     png(filename = "plot1.png")
     with(electric.data, hist(Global_active_power, col = "red", 
                                main = "Global Active Power", 
--- plot4.R
+++ plot4.R
@@ -1,4 +1,7 @@
 plot.4 <- function(file.name = "household_power_consumption.txt"){
+    
+    # Check if needed libraries are loaded
+    
     if ("dplyr" %in% rownames(installed.packages()) == FALSE){
         install.packages("dplyr")
     }
@@ -7,6 +10,20 @@ plot.4 <- function(file.name = "household_power_consumption.txt"){
     }
     library(dplyr)
     library(lubridate)
+    
+    # Check if there are correct file
+    
+    file.name <- "./household_power_consumption.txt"
+    if(!file.exists(file.name)){
+        if(file.exists("./exdata.zip")){
+            unzip("./exdata.zip")
+        }else{
+            stop("Missed files")
+        }
+    }
+    
+    # Load data
+    
     electric.data <- read.table(file.name, header = TRUE, sep = ";", 
                                 na.strings = "?", stringsAsFactors = FALSE)
     electric.data$Date <- as.Date(electric.data$Date, "%d/%m/%Y")
@@ -15,6 +32,9 @@ plot.4 <- function(file.name = "household_power_consumption.txt"){
     electric.data <- mutate(electric.data, Date_Time = 
                                 paste(Date, Time, sep = " "))
     electric.data$Date_Time <- ymd_hms(electric.data$Date_Time)
+    
+    # Create Plot
+    
     png(filename = "plot4.png")
     par(mfcol = c(2,2))
     with(electric.data, plot(Date_Time, Global_active_power, type = "l", 
